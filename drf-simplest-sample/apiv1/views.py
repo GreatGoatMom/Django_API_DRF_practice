65b0f1be56023e7f42355e15be70fcafda174b81
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-08 20:58:19 +0900

    5

--- drf-simplest-sample/apiv1/views.py
+++ drf-simplest-sample/apiv1/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from shop.models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
